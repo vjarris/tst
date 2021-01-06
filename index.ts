e0780fd7088760d48b2a8a944a2536637aea4a06
vjarris <vijay.vasanthapuram@commscope.com>
2021-01-06 00:44:35 -0500

    1 file modified

--- index.ts
+++ index.ts
@@ -1,20 +1,16 @@
 /**
  * Problem:
  * Given a list of strings, return them in their prioritized order.
- * 
- * E.x, 
+ *
+ * E.x,
  * Input:
  * ["MEDIUM", "LOW", "HIGH", "NONE", "HIGH"]
  *
  * Output:
  * ["HIGH", "HIGH", "MEDIUM", "LOW", "NONE"]
- * 
+ *
  */
 
-function sort(values: string[]):  string[]{
-    //Implement
-    return [];
+function sort(values: string[]): string[] {
+  return values;
 }
-
-
-console.log(sort(["HIGH"]));
